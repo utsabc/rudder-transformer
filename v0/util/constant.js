b456356f28463caf7d38e6a09fd71c485b04889c
Arnab <arnab.compsc@gmail.com>
2021-02-11 12:52:10 +0530

    Updated default CACHE TTL for Marketo Auth

--- v0/util/constant.js
+++ v0/util/constant.js
@@ -4,7 +4,7 @@ const USER_LEAD_CACHE_TTL = process.env.MARKETO_LEAD_CACHE_TTL
 
 const AUTH_CACHE_TTL = process.env.MARKETO_AUTH_CACHE_TTL
   ? parseInt(process.env.MARKETO_AUTH_CACHE_TTL, 10)
-  : 60 * 60;
+  : 60;
 
 module.exports = {
   USER_LEAD_CACHE_TTL,
